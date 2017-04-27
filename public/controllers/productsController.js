40f78918193436d08b68f4d201f96eb1f71aac24
Luba Angelova <liuba_angelova@outlook.com>
2017-04-27 21:18:52 +0300

    Add controllers

---
+++ public/controllers/productsController.js
@@ -0,0 +1,39 @@
+var productsController = (function() {
+
+  function all(context) {
+   var products;
+    product-controller.get()
+      .then(function(resProducts) {
+        products = resProducts;
+        return templates.get('products')
+      })
+      .then(function(template) {
+        context.$element().html(template());
+  });   
+  }
+
+    function add(context) {
+    templates.get('product-add')
+      .then(function(template) {
+        context.$element().html(template());     
+
+        $('#btn-product-add').on('click', function() {       
+
+          var product = {
+            title: $('#tb-product-title').val(),
+            description: $('#tb-product-description').val()       
+          };
+
+          product-controller.add(product)
+            .then(function(product) {
+              toastr.success(`Product "${product.title}" added!`);          
+            });
+        });
+    });
+    }
+  
+  return {
+    all: all,
+    add: add
+  };
+}());
